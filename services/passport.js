c0aea4d130880a80c08cfffe5c846de9dfd1e9f1
jehunter5811 <justin.edward.hunter@gmail.com>
2017-09-11 05:51:55 -0500

    Update to passport.js

--- services/passport.js
+++ services/passport.js
@@ -8,6 +8,12 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  })
+});
+
 passport.use(
   new GoogleStrategy(
     {
